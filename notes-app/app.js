22f80300c10b54f8d3b0cac4e0a4a3c0393d0a29
Dmitry <tdmitriiv@gmail.com>
2019-03-17 12:19:54 +0400

    finish chapter 4

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes')
+const notes = require('./notes.js')
 
-yargs.version('1.0.5')
+// Customize yargs version
+yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
@@ -11,11 +12,17 @@ yargs.command({
   builder: {
     title: {
       describe: 'Note title',
-      demandOption: true
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
     }
   },
-  handler: function (argv) {
-    console.log('Adding a new note!', argv)
+  handler (argv) {
+    notes.addNote(argv.title, argv.body)
   }
 })
 
@@ -23,17 +30,24 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler: function () {
-    console.log('Removing the note')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler (argv) {
+    notes.removeNote(argv.title)
   }
 })
 
 // Create list command
 yargs.command({
   command: 'list',
-  describe: 'Listing your notes',
-  handler: function () {
-    console.log('Listing out all notes')
+  describe: 'List your notes',
+  handler () {
+    notes.listNotes()
   }
 })
 
@@ -41,7 +55,16 @@ yargs.command({
 yargs.command({
   command: 'read',
   describe: 'Read a note',
-  handler: function () {
-    console.log('Read a note')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler (argv) {
+    notes.readNote(argv.title)
   }
 })
+
+yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,75 @@
-const getNotes = function () {
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
   return 'Your notes'
 }
 
-module.exports = getNotes
+const addNote = (title, body) => {
+  const notes = loadNotes()
+  const isHasNote = notes.find(note => note.title === title)
+  let msg = ''
+  if (!isHasNote) {
+    notes.push({ title, body })
+    saveNotes(notes)
+    msg = chalk.bgGreen.bold('Note added')
+  } else {
+    msg = chalk.bgRed.bold('Note already exist')
+  }
+  console.log(msg)
+}
+
+const removeNote = (title) => {
+  const notes = loadNotes()
+  const filteredNote = notes.find(note => note.title === title)
+  let msg = ''
+  if (!filteredNote ) {
+    saveNotes(filteredNotes)
+    msg = chalk.bgGreen.bold('Note removed')
+  } else {
+    msg = chalk.bgRed.bold('No found note')
+  }
+  console.log(msg)
+}
+
+const listNotes = () => {
+  const notes = loadNotes()
+  console.log(chalk.bgBlue.bold('Your notes:'))
+  notes.forEach(note =>
+  {
+    console.log(`-${note.title}`)
+  })
+}
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
+
+const readNote = (title) => {
+  const notes = loadNotes()
+  const findedNote = notes.find(note => note.title === title)
+  if (findedNote) {
+    console.log(`${chalk.bgGreen.bold(findedNote.title)} ${findedNote.body}`)
+  } else {
+    console.log(chalk.bgRed.bold('Note not found'))
+  }
+}
+
+module.exports = {
+  getNotes,
+  addNote,
+  removeNote,
+  listNotes,
+  readNote
+}
