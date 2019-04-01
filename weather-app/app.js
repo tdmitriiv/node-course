e4e9b81419bf01e1edaab94bd3c212707b2cf2a8
Дмитрий Трофимов <d.trofimov@kt-team.de>
2019-04-01 18:06:45 +0400

    destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,17 +3,17 @@ const forecast = require('./utils/forecast')
 
 const location = process.argv[2]
 
-geoCode(location || 'Philadelphia', (error, data) => {
+geoCode(location || 'Philadelphia', (error, { latitude, longitude, location }) => {
   if (error) {
     return console.error('error', error)
   }
 
-  forecast(data.latitude, data.longitude, (error, forecasrData) => {
+  forecast(latitude, longitude, (error, forecasrData) => {
     if (error) {
       return console.error('error', error)
     }
 
-    console.log(data.location)
+    console.log(location)
     console.log(forecasrData)
   })
 })
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,17 +2,17 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/a9387689aca3e72f7f63e2a82d5fb2da/${latitude},${longitude}?lang=ru`
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service', undefined)
       return
     }
-    if (response.body.error) {
-      callback(response.body.error, undefined)
+    if (body.error) {
+      callback(body.error, undefined)
       return
     }
-    const currently = response.body.currently
-    const daily = response.body.daily.data[0]
+    const currently = body.currently
+    const daily = body.daily.data[0]
     callback(undefined, `${daily.summary} It is currently ${currently.temperature} degrees out. There is a ${currently.precipIntensity}% chance of rain.`)
   })
 }
