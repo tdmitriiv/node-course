23c25e676049f4cbfb7931ea633090ed6eaae573
Дмитрий Трофимов <d.trofimov@kt-team.de>
2019-04-09 16:28:02 +0400

    Html pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send('hello express')
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page 2')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About page')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
-  res.send('Weather app')
+  res.send({
+    forecast: 'Is it snowing',
+    location: 'Denver'
+  })
 })
 
 app.listen(3030, () => {
