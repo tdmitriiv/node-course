6fc4b7185004571d9c2d24309b12cd6747811fc2
Дмитрий Трофимов <d.trofimov@kt-team.de>
2019-04-12 17:08:47 +0400

    templating hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Dmitry'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about',{
+    title: 'About me',
+    name: 'Dmitry'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('about',{
+    title: 'Help',
+    name: 'Some message'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
@@ -13,6 +35,6 @@ app.get('/weather', (req, res) => {
   })
 })
 
-app.listen(3030, () => {
-  console.log('Server is up on port 3030')
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
 })
