310eb132172cec84d869ac509ebf59f0d4926c10
Дмитрий Трофимов <d.trofimov@kt-team.de>
2019-04-17 13:39:14 +0400

    partials and 404 view

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewPath = path.join(__dirname, '../templates')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebar endgine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
+hbs.registerPartials(partialPath)
 
 // Setup static deirectory to serve
 app.use(express.static(publicDirectoryPath))
@@ -29,19 +32,36 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/help', (req, res) => {
-  res.render('about',{
+  res.render('help',{
     title: 'Help',
-    name: 'Some message'
+    message: 'Some message',
+    name: 'Dmitry'
   })
 })
 
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'Is it snowing',
-    location: 'Denver'
+    location: 'Denver',
   })
 })
 
-app.listen(3030, () => {
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 'Error',
+    name: 'Dmitry',
+    message: 'Help article not found'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: 'Error',
+    name: 'Dmitry',
+    message: 'Page not found'
+  })
+})
+
+app.listen(3000, () => {
   console.log('Server is up on port 3000')
 })
